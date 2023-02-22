e7325b6e7e716464faac79ee38d76fc0a2d92461
jfsong100 <jsong100@jhu.edu>
2023-02-21 22:36:38 -0500

    add test mle finder file

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -10,3 +10,15 @@ test_that("linalg and optim least-sq coincide", {
     coef(via_linalg_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
   ))
 })
+
+test_that("gradient calculation by comparing it against a numerical one", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  test_MLE = rep(1,n_pred)
+  expect_true(are_all_close(
+    linear.gradient(test_MLE,design,outcome,noise_var = 1),
+    approx_grad(linear.loglik,test_MLE,design,outcome),
+    abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
