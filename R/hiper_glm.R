7c3346c643ca19dd6b08a6507a1de648a01996f9
jfsong100 <jsong100@jhu.edu>
2023-02-21 22:43:45 -0500

    implement MLE in linear model

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -1,11 +1,20 @@
 #' @export
-hiper_glm <- function(design, outcome, model = "linear"){
+hiper_glm <- function(design, outcome, model = "linear", option=list()){
   supported_model <- c("linear", "logit")
   if(!(model %in% supported_model)){
-    stop(sprintf(""))
+    stop(sprintf("Model is not available"))
   }
   # TODO: find MLE.
-  hglm_out <- list()
+  if(model == "linear"){
+    if(is.null(option$mle_solver)==TRUE){
+      MLE = MLE_peudo.inverse(design,outcome)
+    }
+    else if(option$mle_solver=='BFGS'){
+      MLE = linear.BFGS(design,outcome,noise_var = 1)$par
+    }
+  }
+
+  hglm_out <- list(coef=MLE)
   class(hglm_out) <- "hglm"
   return(hglm_out)
 }
